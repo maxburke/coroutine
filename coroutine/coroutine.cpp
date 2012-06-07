eb21c021895cf725518bb90b09187619099c2da1
Max Burke <maxburke@gmail.com>
2012-06-06 21:23:18 -0700

    Removed unnecessary switch statement.

--- coroutine/coroutine.cpp
+++ coroutine/coroutine.cpp
@@ -42,35 +42,30 @@ enum coroutine_status coroutine_resume(struct coroutine *co)
             || co->header.status == COROUTINE_STATUS_EXECUTING)
         return co->header.status;
 
-    switch (setjmp(co->header.calling_context))
+    if (setjmp(co->header.calling_context) == 0)
     {
-        case 0:
+        // New co-routines require setting up our new "stack" in the 4kb block we've allocated.
+        // The co-routine is called like a normal function and when it returns it will have
+        // finished successfully.
+        if (co->header.status == COROUTINE_STATUS_NEW)
         {
-            // New co-routines require setting up our new "stack" in the 4kb block we've allocated.
-            // The co-routine is called like a normal function and when it returns it will have
-            // finished successfully.
-            if (co->header.status == COROUTINE_STATUS_NEW)
-            {
-                register unsigned int newStack = (unsigned int)&co->header;
-                __asm mov esp, newStack
-                co->header.status = COROUTINE_STATUS_EXECUTING;
-                co->header.return_value = co->header.function(co->header.parameter);
-                co->header.status = COROUTINE_STATUS_FINISHED;
+            register unsigned int newStack = (unsigned int)&co->header;
+            __asm mov esp, newStack
+            co->header.status = COROUTINE_STATUS_EXECUTING;
+            co->header.return_value = co->header.function(co->header.parameter);
+            co->header.status = COROUTINE_STATUS_FINISHED;
 
-                // Restoring our stack pointer is done by longjmp'ing back to our calling context.
-                // This saves us from having to properly save and restore the stack pointer and,
-                // since we're carrying around that state anyways, we might as well use it.
-                longjmp(co->header.calling_context, 1);
-            }
-            else
-            {
-                // If we're not a new invocation then we're going to resume our co-routine by
-                // longjmp'ing back to where we were.
-                longjmp(co->header.coroutine_context, 1);
-            }
+            // Restoring our stack pointer is done by longjmp'ing back to our calling context.
+            // This saves us from having to properly save and restore the stack pointer and,
+            // since we're carrying around that state anyways, we might as well use it.
+            longjmp(co->header.calling_context, 1);
         }
-        default:
-            break;
+        else
+        {
+            // If we're not a new invocation then we're going to resume our co-routine by
+            // longjmp'ing back to where we were.
+            longjmp(co->header.coroutine_context, 1);
+        }
     }
 
     return co->header.status;
